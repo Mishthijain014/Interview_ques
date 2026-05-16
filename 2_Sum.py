e515c07f949c3b752e0374df8056fd0c339315e4
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-05-16 15:18:00 +0530

    2sum

---
+++ 2_Sum.py
@@ -0,0 +1,18 @@
+def two_sum(num,target):
+    left = 0
+    right = len(num) - 1
+
+    while left < right:
+        targeted_sum = num[left] + num[right]
+
+        if targeted_sum == target:
+            return[left+1,right+1]
+            break
+
+        elif targeted_sum < target:
+            left += 1
+
+        else:
+            right -= 1
+
+print(two_sum([2,7,11,15], 9))
