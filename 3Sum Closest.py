87d174c1e516dac5329411140dacc84fcb7058de
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-05-17 23:11:43 +0530

    '3sumclosest

---
+++ 3Sum Closest.py
@@ -0,0 +1,31 @@
+def three_Sum_Closest(nums,target):
+    nums.sort()
+
+    closest_sum = nums[0] + nums[1] + nums[2]
+
+    for i in range(len(nums)-2):
+
+        left = i+1
+        right = len(nums) - 1
+
+        while left < right:
+            
+            curr_sum =  nums[i] + nums[left] + nums[right]
+
+            if abs(target - curr_sum) < abs(target - closest_sum):
+                closest_sum = curr_sum
+
+            if curr_sum < target:
+                left += 1
+
+            elif curr_sum > target:
+                right -= 1
+
+            else:
+                return closest_sum
+            
+    return closest_sum
+
+print(three_Sum_Closest([-1,2,1,-4],1))
+
+       
