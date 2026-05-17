01e6d523db7fba7ca3c055207eb07c42543d0cbc
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-05-17 23:43:41 +0530

    pow1

--- Pow(x, n).py
+++ Pow(x, n).py
@@ -1,6 +1,6 @@
 def Pow(x, n):
     if(n==0):
-        return 0
+        return 1
     if(n==1):
         return x
     if(n<0):
@@ -9,11 +9,12 @@ def Pow(x, n):
 
     half = pow(x,n//2)
 
-    if x%2==0:
+    if n%2==0:
         return half * half
     else:
         return x * half * half
     
 print(pow(2,3))
 
-    
+# time complexity = O(logn)
+# space complexity = 0(logn) 
